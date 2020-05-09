8035fec55b7b8259be1def8460f1eabbeffe3433
ArevSuleymanyan <65043633+ArevSuleymanyan@users.noreply.github.com>
2020-05-09 21:28:42 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+function operand(num){
+    document.getElementById('input').value = 
+    document.getElementById('input').value+num;
+}
+
+function clean(){
+    document.getElementById('input').value = '';
+}
+
+function back(){
+    let sub = document.getElementById('input').value;
+    document.getElementById('input').value = sub.substring(0, (sub.length-1))
+}
+
+function equal(){
+    let number = document.getElementById('input').value;
+    if(number){
+        document.getElementById('input').value = eval(number);   
+    }
+}
